878d0612fc26ed5e37117a65ce3d618bfb6f37fa
Pawan Joshi <business.pawanjoshi@gmail.com>
2024-02-27 17:31:59 +0530

    some change in Not Found page

--- src/Components/Notfount/NotFound.jsx
+++ src/Components/Notfount/NotFound.jsx
@@ -15,7 +15,7 @@ const NotFound = () => {
             <div className="contant_box_404">
               <h3 className="h2">Look like you're lost</h3>
               <p>The page you are looking for is not available!</p>
-              <Link to="/" className="link_404">Go to Home</Link>
+              <Link to="/" className="link_404">Go to Home </Link>
             </div>
           </div>
         </div>
